e483b8999c4868c7493a79238e98df2f64c5faba
Sriram7849 <sriramspartan10@gmail.com>
2022-05-04 11:29:40 +0530

    adding new project

---
+++ src/org/dayone/Task1.java
@@ -0,0 +1,17 @@
+package org.dayone;
+
+public class Task1 {
+	
+	public void task() {
+System.out.println("task1");
+	}
+	public void task2() {
+System.out.println("task2");
+	}
+	public static void main(String[] args) {
+		Task1 a = new Task1();
+		a.task();
+		a.task2();
+	}
+
+}
